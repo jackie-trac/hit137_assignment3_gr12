e4f49309e91d5d0feb440cd280c18c08f0f11352
dpjericho <163237814+dpjericho@users.noreply.github.com>
2024-10-12 17:18:42 +0930

    Question 2 Folder and initial pygame python file
    Kindly push/commit your contributions here.

---
+++ Question2/pygame-v1.py
@@ -0,0 +1,25 @@
+# This is the starting pygame python file. Kindly push/commit your revisions here.
+
+import pygame
+import math
+
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    K_x,
+    KEYDOWN,
+    KEYUP,
+    QUIT,
+)
+
+up = False
+down = False
+left = False
+right = False
+faced_left = True
+Shoot = False
+
